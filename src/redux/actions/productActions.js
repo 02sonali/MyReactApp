777f8d7e0ae2c9193dabaa1886e9f2c846532ab6
unknown <02sonali@gmail.com>
2020-05-23 02:04:22 +0530

    fixed typos

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -1,4 +1,4 @@
-import * as types from './productActions';
+import * as types from './actionTypes';
 
 export function createProduct(product) {
     return {type: types.CREATE_PRODUCT, product: product}; //or write product only {type: 'CREATE_PRODUCT', product}
